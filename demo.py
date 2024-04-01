ff9a4c6a95c27753162beebaac33e9e0f7220e5e
Rahul-Jain-1 <jainshabrahul@gmail.com>
2024-04-01 23:51:06 +0530

    done

--- demo.py
+++ demo.py
@@ -1,11 +1,13 @@
 from us_visa.logger import logging
 from us_visa.exception import USvisaException
+# /config/workspace/us_visa/exception
 import sys
 # custom log
 # logging.info("This is the custom log file")
 
 # how it will show the error 
 try:
-    a = 1 / "10"
+    a = 1 / 1
 except Exception as e:
+    logging.info(e)
     raise USvisaException(e,sys) from e
